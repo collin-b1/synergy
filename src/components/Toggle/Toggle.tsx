d7087839769b8e5a962cf9c3f662e762c3576431
collin-b1 <github.1wjd0@simplelogin.com>
2024-02-26 22:03:36 -0500

    Update toggle button

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -15,7 +15,7 @@ export const Toggle: React.FC<ToggleProps> = props => {
         defaultChecked={toggled}
         {...rest}
       />
-      <div className="peer h-6 w-11 rounded-full bg-gray-500 after:absolute after:left-[2px] after:top-[2px]  after:h-5  after:w-5 after:rounded-full after:border after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-focus:outline-none peer-focus:ring-4  peer-focus:ring-blue-300"></div>
+      <div className="peer h-6 w-11 rounded-full bg-gray-500 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300"></div>
     </label>
   );
 };
